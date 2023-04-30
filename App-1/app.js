085a2877a6f2ac8ab1faca54f3413601120af409
praveen <mspraveenkumar77@gmail.com>
2023-04-30 19:22:16 +0530

    Update

--- App-1/app.js
+++ App-1/app.js
@@ -1,32 +1,25 @@
-// const http = require("http");
 const express = require("express");
 const app = express();
-// const routes = require("./routes");
-// const server = http.createServer(routes.handler);
-// app.use((req, res, next) => {
-//   console.log("This is a middleware and this will executed for every incoming request");
-//   next(); // Allows the request to continue to the next middleware in line
-// });
-app.use('/', (req, res, next) => {
-  console.log("This always runs!!");
-  next();
-});
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: false})); // This is a middleware, which will parse the body of the incoming request and add it to the req object
+                                  // urlencoded() has next() in it, so it will call next() automatically
 app.use('/add-product', (req, res, next) => {
-  console.log('The "Add Product" middleware');
-  res.setHeader("Content-Type", "text/html"); // Optional, But Express will set it by default
-  res.send("<h1>The Add Product Page!!<h1>");
+  // res.setHeader("Content-Type", "text/html"); // Optional, But Express will set it by default
+  res.send('<form method="POST" action="/product"><input type="text" name="title"><button type="submit">Add Products</button></form>');
+});
+
+app.post('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-  console.log("This will executed for every incoming request_2");
-  res.setHeader("Content-Type", "text/html"); // Optional, But Express will set it by default
+  // console.log("This will executed for every incoming request_2");
+  res.setHeader("Content-Type", "text/html");
   res.send("<h1>Hello from Express!!<h1>");
 });
-// const server = http.createServer(app);
-// server.listen(4000, () => {
-//   console.log("Server is listening on port 4000");
-// });
+
 app.listen(4000, () => {
   console.log("Server is listening on port 4000");
 });
