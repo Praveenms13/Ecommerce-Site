e23e34eedc4195fcd9078310b7b56362ded93453
praveen <mspraveenkumar77@gmail.com>
2023-05-06 21:47:00 +0530

    AddedExpressJS

---
+++ app.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false })); // This is a middleware, which will parse the body of the incoming request and add it to the req object, //urlencoded() has next() in it, so it will call next() automatically
+app.use('/admin', adminRoutes)
+app.use(shopRoutes)
+// I am using router.get in admin and in shop so that I can change the middlewear, but if I use app.get, then I can't change the middlewear
+
+// 404 page
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
+app.listen(4000, () => {
+  console.log("Server is listening on port 4000");
+});
