2e002f5888d9eaab467cd11906202289a53ca6fa
praveen <mspraveenkumar77@gmail.com>
2023-06-10 19:30:09 +0530

    Updated-UI

--- models/product.js
+++ models/product.js
@@ -25,7 +25,7 @@ const getProductsFromFile = (cb) => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(title, imageUrl, price, description) {
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
